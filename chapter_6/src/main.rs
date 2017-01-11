1dfe56c6b5436f355c43fa5754920cef8eed7f6d
Ty <Ty_Coghlan@hms.harvard.dedu>
2017-01-11 15:17:38 -0500

    fixed from tuple

--- chapter_6/src/main.rs
+++ chapter_6/src/main.rs
@@ -18,7 +18,7 @@ impl Expression {
         Ok(Expression::Number(u))
     }
 
-    fn from_tuples(op: Operator, exprs: Vec<Self>) -> Result<Self, ()> {
+    fn from_tuple((op, exprs): (Operator, Vec<Expression>)) -> Result<Self, ()> {
         if exprs.is_empty() {
             Err(())
         } else {
@@ -57,7 +57,7 @@ named!(
     operator_parser<Operator>,
     map_res!(
         one_of!("*+/-"),
-        Operator::from_char)); 
+        Operator::from_char));
 
 named!(
     number_parser<Expression>,
@@ -68,18 +68,18 @@ named!(
                 from_utf8),
             FromStr::from_str),
         Expression::from_usize));
-    
-named!(
-    expression_parser<Expression>,
-        alt!(
-            number_parser | 
-            map_res!(
-                do_parse!(
-                    operator: operator_parser >>
-                    tag!(" ") >>
-                    expression_vec: ws!(delimited!(tag!("(", many1!(expression_parser), tag!(")")))) >> 
-                    (operator, expression_vec))
-                Expression::from_tuple)));
+
+// named!(
+//     expression_parser<Expression>,
+//         alt!(
+//             number_parser |
+//             map_res!(
+//                 do_parse!(
+//                     operator: operator_parser >>
+//                     tag!(" ") >>
+//                     expression_vec: ws!(delimited!(tag!("(", many1!(expression_parser), tag!(")")))) >>
+//                     (operator, expression_vec))
+//                 Expression::from_tuple)));
 
 named!(
     program_start_parser<Expression>,
@@ -90,7 +90,7 @@ named!(
             expression_vec: ws!(many1!(expression_parser)) >>
             (operator, expression_vec)),
         Expression::from_tuple));
-            
+
 
 fn main() {
 
@@ -124,18 +124,18 @@ fn number_parser_test() {
 fn expression_parser_test() {
    assert_eq!(output(expression_parser(" 1".as_bytes())), Expression::Number(1));
    assert_eq!(output(program_start_parser("+ 1 2 3".as_bytes())), Expression::Expr(Operator:: Plus, vec![Expression::Number(1), Expression::Number(2), Expression::Number(3)]));
-   assert_eq!(output(program_start_parser("+ (* (- 7 1) 2 3) 2 (/ 3 3)".as_bytes())), 
+   assert_eq!(output(program_start_parser("+ (* (- 7 1) 2 3) 2 (/ 3 3)".as_bytes())),
               Expression::Expr(
                   Operator:: Plus,
                   vec![
                   Expression::Expr(
-                      Operator::Mul, 
+                      Operator::Mul,
                       vec![
                       Expression::Expr(
-                          Operator::Sub, 
+                          Operator::Sub,
                           vec![
-                          Expression::Number(7), 
-                          Expression::Number(1)]), 
+                          Expression::Number(7),
+                          Expression::Number(1)]),
                       Expression::Number(2),
                       Expression::Number(3)]),
                   Expression::Number(2),
@@ -145,5 +145,4 @@ fn expression_parser_test() {
                       Expression::Num(3),
                       Expression::Num(3)])]));
 
-                 
 }
